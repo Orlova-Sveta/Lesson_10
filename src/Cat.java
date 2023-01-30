1b6395fa186a967e71ae5de28f0373921a9ad832
Orlova-Svetlana <orlovaS1999@yandex.ru>
2023-01-30 21:06:52 +0300

    4) Параметризованные типы

--- src/Cat.java
+++ src/Cat.java
@@ -1,5 +1,14 @@
-public class Cat extends Animal {
+public class Cat<T> extends Animal {
+    T info;
     public void test1() {
         System.out.println("Это я, кошечка");
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,60 +4,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //Wildcard - это особая конструкция, которая позволяет изменить логику Generics
-        // и сделать их не только инвариантными,
-        // но и ковариантными и контравариантными.
-        // Для этого используется конструкция ? extends или ? super
-        List<Integer> i = Arrays.asList(1,2,3,4);
-        List<? extends Number> n = i; // как реализация ковариантности
+        String test = "gfdg";
+        Integer inn = 5;
+        List<Integer> list = new ArrayList<>();
 
-        List<Number> n2 = new ArrayList<>();
-        List<? super Integer> i2  = i; // как реализация контравариантными
+        Cat<Integer> integerCat = new Cat<>();
+        integerCat.setInfo(5);
+        System.out.println(integerCat.getInfo());
 
+        Cat<String> stringCat = new Cat<>();
+        stringCat.setInfo("dskhfksw");
+        System.out.println(stringCat.getInfo());
 
-        /**
-         * The Get and Put Principle или PECS (Producer Extends Consumer Super)
-         * Это означает, что ценой удобства Generics мы платим ограничением
-         * на вставку или добавление элементов
-         *
-         * extends - данный модификатор для дженерика означит,
-         * что данный тип являетcя producer.
-         * Данный элемент продюсирует свои данные (предоставляет) но не изменяет или добавляет их
-         *
-         * super - данный модификатор создаёт Consumer, который является неким читателем.
-         * Он получает данные, но не отдаёт их
-         */
+        Cat<MiniCat> miniCatCat = new Cat<>();
+        MiniCat miniCat = new MiniCat();
 
-        List<Integer> integerList = new ArrayList<>();
-        integerList.add(5);
-        integerList.add(10);
-        integerList.add(13);
-        integerList.add(18);
-
-        System.out.println(integerList.get(2));
+        miniCatCat.setInfo(miniCat);
+        miniCatCat.getInfo().test1();
 
     }
 
-    /**
-     * Пример работы Producer
-     * Где мы можем получать элемнты, но не добалвять мх
-     * @param numbers
-     */
-    private static void test(List<? extends Number> numbers) {
-        System.out.println(numbers.get(3));
-        numbers.add(3);
-    }
 
-    /**
-     * Пример работы Consumer.
-     * Где мы можем добавлять элле менты, но не получать их
-     */
-    public static Integer getElement(List<? super Integer> list) {
-        list.add(4);
-        System.out.println(list);
-        return list.get(1);
+    public static <E extends TestInf> Integer getValue(E value) {
+        System.out.println(value);
+        return value.test();
     }
-
 
 
 
